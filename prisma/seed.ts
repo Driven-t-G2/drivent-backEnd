77d2079d0c3a069c2842c8152234bdafa497e96e
Felipe Bueno <felipebueno2201@gmail.com>
2023-03-05 16:26:36 -0300

    seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,50 @@
-import { PrismaClient } from "@prisma/client";
-import dayjs from "dayjs";
+import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
+
 const prisma = new PrismaClient();
+const now = dayjs().toDate();
 
 async function main() {
   let event = await prisma.event.findFirst();
   if (!event) {
     event = await prisma.event.create({
       data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
-        startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
+        title: 'Driven.t',
+        logoImageUrl: 'https://files.driveneducation.com.br/images/logo-rounded.png',
+        backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
+        startsAt: now,
+        endsAt: dayjs(now).add(21, 'days').toDate(),
       },
     });
   }
 
   console.log({ event });
+
+  await prisma.ticketType.createMany({
+    data: [
+      {
+        name: 'Evento Remoto',
+        price: 100,
+        isRemote: true,
+        includesHotel: false,
+        updatedAt: now,
+      },
+      {
+        name: 'Evento Presencial',
+        price: 150,
+        isRemote: false,
+        includesHotel: false,
+        updatedAt: now,
+      },
+      {
+        name: 'Evento com Hospedagem',
+        price: 250,
+        isRemote: false,
+        includesHotel: true,
+        updatedAt: now,
+      },
+    ],
+  });
 }
 
 main()
