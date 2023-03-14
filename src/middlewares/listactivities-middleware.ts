bec7e676704c6b1c7f8aaa4e94597de1a417093f
Pedro-Arantes <developer9pedro@gmail.com>
2023-03-14 17:16:40 -0300

    feat: listactivities middleware and routes

---
+++ src/middlewares/listactivities-middleware.ts
@@ -0,0 +1,35 @@
+import { prisma } from "@/config";
+import { notFoundError } from "@/errors";
+import { cannotListHotelsError } from "@/errors/cannot-list-hotels-error";
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import ticketRepository from "@/repositories/ticket-repository";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { AuthenticatedRequest } from "./authentication-middleware";
+
+export async function listActivitiesMd(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { userId } = req;
+  //Tem enrollment?
+  try {
+    const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+    if (!enrollment) {
+      throw notFoundError();
+    }
+    //Tem ticket pago isOnline false e includesHotel true
+    const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+
+    if (!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+      throw cannotListHotelsError();
+    }
+
+    return next();
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.sendStatus(httpStatus.NOT_FOUND);
+    }
+    if (error.name === "cannotListHotelsError") {
+      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    }
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
